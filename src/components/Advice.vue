0215b5e5362a4cbf023a2420069fe4a0fa77c77e
Farhia <farhia.fcb@gmail.com>
2020-06-21 17:51:45 +0200

    Import axios and call advice API :construction:

---
+++ src/components/Advice.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="advice">
+    <button v-on:click="getAdvice()">Give me talo</button>
+    <div v-if="talo">
+      {{talo}}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+ name: 'Advice',
+ data() {
+   return {
+     talo: ''
+   }
+ },
+ methods:{
+   getAdvice(){
+     axios.get('https://api.adviceslip.com/advice').then(response => {
+       this.talo = response.data.slip.advice;
+      });
+   }
+ }
+}
+</script>
+
+<style>
+
+</style>
